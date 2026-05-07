95ea14b8cced32ec44d744439029a016d2be8542
B1tR0s3s <231447@unsaac.edu.pe>
2026-05-07 08:09:32 -0500

    Anadir validacion de los inputs

--- script.js
+++ script.js
@@ -13,14 +13,51 @@ function saludar() {
             "Hola " + nombre + ", bienvenido al sistema.";
     }
 }
-function validarCorreo() {
-    let correo = document.getElementById("correo").value;
 
-    if (correo === "") {
-        document.getElementById("mensajeCorreo").innerText =
-            "Debe ingresar un correo.";
+function validarCorreo() {
+    let nombre = document.getElementById("nombreContacto").value;
+    let apellido = document.getElementById("apellido").value;
+    let telefono = document.getElementById("telefono").value;
+    let correo = document.getElementById("correo").value;
+    let mensaje = document.getElementById("mensaje").value;
+    
+    let mensajeDiv = document.getElementById("mensajeCorreo");
+
+    if (nombre === "") {
+        mensajeDiv.innerText = "Debe ingresar su nombre.";
+        mensajeDiv.className = "mensaje error";
+    } else if (apellido === "") {
+        mensajeDiv.innerText = "Debe ingresar su apellido.";
+        mensajeDiv.className = "mensaje error";
+    } else if (telefono === "") {
+        mensajeDiv.innerText = "Debe ingresar su teléfono.";
+        mensajeDiv.className = "mensaje error";
+    } else if (telefono.length < 9) {
+        mensajeDiv.innerText = "El teléfono debe tener al menos 9 dígitos.";
+        mensajeDiv.className = "mensaje error";
+    } else if (correo === "") {
+        mensajeDiv.innerText = "Debe ingresar un correo.";
+        mensajeDiv.className = "mensaje error";
+    } else if (!correo.includes("@") || !correo.includes(".")) {
+        mensajeDiv.innerText = "Ingrese un correo válido.";
+        mensajeDiv.className = "mensaje error";
+    } else if (mensaje === "") {
+        mensajeDiv.innerText = "Debe escribir un mensaje.";
+        mensajeDiv.className = "mensaje error";
+    } else if (mensaje.length < 10) {
+        mensajeDiv.innerText = "El mensaje debe tener al menos 10 caracteres.";
+        mensajeDiv.className = "mensaje error";
     } else {
-        document.getElementById("mensajeCorreo").innerText =
-            "Correo registrado correctamente.";
+        mensajeDiv.innerText = "Formulario registrado correctamente.";
+        mensajeDiv.className = "mensaje exito";
+
+        document.getElementById("formularioContacto").reset();
     }
 }
+
+document
+    .getElementById("formularioContacto")
+    .addEventListener("submit", function(evento) {
+        evento.preventDefault();
+        validarCorreo();
+    });
